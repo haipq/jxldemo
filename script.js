3582448c2be5f9791674f53fd477ea52a28646a7
haipq <haipq@vng.com.vn>
2023-06-19 16:33:14 +0700

    add src test

--- script.js
+++ script.js
@@ -1,31 +1,10 @@
 const fs = require('fs');
-fs.readdir('./img/jxl', (err, files) => {
+fs.readdir('./img', (err, files) => {
     const entries = [];
     files.forEach(fileName => {
+        if (!fileName.endsWith('jxl')) return;
         let fn = fileName.replace('.jxl', '');
-        entries.push('/set/' + fn + '.html');
-        fs.writeFileSync('./set/' + fn + '.html', `
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width">
-<title>JXL.js Demo</title>
-<link rel="prefetch" href="jxl_dec.js" fetchpriority="high">
-<link rel="prefetch" href="jxl_dec.wasm" fetchpriority="high">
-<script src="jxl.min.js"></script>
-</head>
-<body>
-    <h1><a href="https://github.com/niutech/jxl.js" style="color: inherit;">JXL.js</a> demo</h1>
-    <div style="display: grid; grid-template-columns: 1fr 1fr; grid-gap: 10px;">
-    <div>JXL</div>
-    <div>JPEG</div>
-    <img style="max-width: 100%;" src="/img/${fn}.jxl" alt="Here is the image">
-    <img style="max-width: 100%;" src="/img/${fn}.jpeg" alt="Here is the image">
-    </div>
-</body>
-</html>`
-        );
+        entries.push(fn);
     });
     fs.writeFileSync('./index.html', `
     <!DOCTYPE html>
@@ -40,7 +19,17 @@ fs.readdir('./img/jxl', (err, files) => {
     </head>
     <body>
         <h1>JXL demo</h1>
-        ${entries.map(entry => `<a href="${entry}" target="_blank">${entry}</a>`).join('<br/>')}
+        ${entries.map(entry => `<button onclick="(function(){
+            document.getElementById('wrapper').innerHTML = ${`\`
+            <div style='display: grid; grid-template-columns: 1fr 1fr; grid-gap: 10px;'>
+            <div>JXL</div>
+            <div>JPEG</div>
+            <img style='max-width: 100%;' src='img/${entry}.jxl' alt='Here is the image'>
+            <img style='max-width: 100%;' src='img/${entry}.jpg' alt='Here is the image'>
+            </div>\``}
+            return false;
+        })();return false;">${entry}</button>`).join('\n')}
+        <div id="wrapper"></div>
     </body>
     </html>`)
 })
